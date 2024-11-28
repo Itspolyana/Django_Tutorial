3ebdadd93a65cfdd36c5307a76e7a48bbeb850cc
Polyana Albanaz <polyana.albanaz.ifc@gmail.com>
2024-11-28 09:43:34 -0300

    Tutorial Django REST 03

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,9 @@
-from django.contrib import admin
+# config/urls.py
+from django.contrib import admin 
 from django.urls import include, path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Bem-vindo à página inicial!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
-    path('', home, name='home'),  # Rota para a raiz
+    path('api-auth/', include('rest_framework.urls')),
 ]
